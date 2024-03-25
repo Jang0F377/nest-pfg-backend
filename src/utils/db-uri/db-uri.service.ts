4f41be9b3b6255a76baa34688a8179e48c22cb01
Matt Garrett <mjgarrett7092@gmail.com>
2024-03-25 15:59:22 -0700

    feat: updated deploy.yaml

--- src/utils/db-uri/db-uri.service.ts
+++ src/utils/db-uri/db-uri.service.ts
@@ -21,7 +21,7 @@ export class DbUriService {
     const user = db.username;
     const pass = db.password;
     let uri: string;
-    this.logger.error('DB', db);
+    console.log('DB', db);
 
     if (user && pass) {
       uri = `mongodb://${user}:${pass}@${db.host}:${db.port}/${
@@ -30,7 +30,7 @@ export class DbUriService {
     } else {
       uri = `mongodb://${db.host}:${db.port}/${db.database}`;
     }
-    this.logger.error(uri);
+    console.log(uri);
     return uri;
   }
 }
